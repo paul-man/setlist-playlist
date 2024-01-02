52dcbe8e9bcf97ef2b5b6e16596c17668b99253f
conkur <kevinjyoungjr@gmail.com>
2024-01-02 10:59:19 -0500

    feat(nav): adds side-bar to app

---
+++ src/app/account/page.tsx
@@ -0,0 +1,9 @@
+import {SpotifyLoginButton} from '@/components/SpotifyLoginButton';
+
+export default function Account() {
+    return (
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <SpotifyLoginButton />
+      </div>
+    )
+}
---
+++ src/app/discover/page.tsx
@@ -0,0 +1,7 @@
+export default function Discover() {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        `Find a Setlist` and `Trending on Sputnik` options can go here
+    </div>
+  )
+}
